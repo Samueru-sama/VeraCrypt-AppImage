2bc333e4d34d6dc641434957bf2b524aa1475a60
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-04 20:28:39 -0400

    Update template and hook (#11)
    * Update README.md
    
    * Update make-appimage.sh
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Link4Electronics <link4electronics@gmail.com>

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q veracrypt | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/veracrypt/VeraCrypt/refs/heads/master/src/Resources/Icons/VeraCrypt-256x256.png
 export DESKTOP=https://raw.githubusercontent.com/veracrypt/VeraCrypt/refs/heads/master/src/Setup/Linux/veracrypt.desktop
